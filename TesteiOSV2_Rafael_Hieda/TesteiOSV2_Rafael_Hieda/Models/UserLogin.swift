a72cb50f7bae4ad6368138f45c8956e6bf572db3
Rafael <41241789@mackenzista.com.br>
2018-10-27 13:46:48 -0300

    Inclusão de dependências no projeto. As dependências incluídas são : Alamofire para realizar Post/Get requests, AlamofireObjectMapper como Object Relational Mapping das requisições a serem realizadas.

---
+++ TesteiOSV2_Rafael_Hieda/TesteiOSV2_Rafael_Hieda/Models/UserLogin.swift
@@ -0,0 +1,12 @@
+//
+//  UserLogin.swift
+//  TesteiOSV2_Rafael_Hieda
+//
+//  Created by Rafael  Hieda on 10/27/18.
+//  Copyright © 2018 Rafael Hieda. All rights reserved.
+//
+
+import UIKit
+
+class UserLogin: NSObject {
+}
