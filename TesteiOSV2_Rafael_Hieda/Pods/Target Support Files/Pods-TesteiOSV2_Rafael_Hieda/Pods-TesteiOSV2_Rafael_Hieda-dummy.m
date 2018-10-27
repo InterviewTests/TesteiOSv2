21abd33133300aebd18cae39df3a6a679eb963d1
Rafael <41241789@mackenzista.com.br>
2018-10-27 13:48:17 -0300

    correção de inclusão de dependencias

---
+++ TesteiOSV2_Rafael_Hieda/Pods/Target Support Files/Pods-TesteiOSV2_Rafael_Hieda/Pods-TesteiOSV2_Rafael_Hieda-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TesteiOSV2_Rafael_Hieda : NSObject
+@end
+@implementation PodsDummy_Pods_TesteiOSV2_Rafael_Hieda
+@end
---
+++ TesteiOSV2_Rafael_Hieda/Pods/Target Support Files/Pods-TesteiOSV2_Rafael_HiedaUITests/Pods-TesteiOSV2_Rafael_HiedaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TesteiOSV2_Rafael_HiedaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TesteiOSV2_Rafael_HiedaUITests
+@end
