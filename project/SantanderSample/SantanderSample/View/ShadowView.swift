370b7cfbda9062ecd1d023f0c63a4287d1718c87
Virgilius Santos <virgilius_santos@yahoo.com.br>
2018-10-27 04:35:53 -0300

    add show a tableviewcell

---
+++ project/SantanderSample/SantanderSample/View/ShadowView.swift
@@ -0,0 +1,27 @@
+//
+//  ShadowView.swift
+//  SantanderSample
+//
+//  Created by Virgilius Santos on 27/10/18.
+//  Copyright © 2018 Virgilius Santos. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    override var bounds: CGRect {
+        didSet {
+            setupShadow()
+        }
+    }
+    
+    private func setupShadow() {
+        self.layer.cornerRadius = 5
+        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+        self.layer.shadowRadius = 5
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 5, height: 5)).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+    }
+}
