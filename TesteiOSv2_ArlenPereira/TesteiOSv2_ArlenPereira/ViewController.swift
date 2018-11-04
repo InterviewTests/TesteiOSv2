f242841d55fee2f0c6d20cbf0884f8969a813d73
ArlenPereira <arlen.r.pereira@gmail.com>
2018-11-03 22:27:29 -0300

    Initial configurations: Create Swift project and cocoa pods.

---
+++ TesteiOSv2_ArlenPereira/TesteiOSv2_ArlenPereira/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOSv2_ArlenPereira
+//
+//  Created by Arlen Ricardo Pereira on 03/11/18.
+//  Copyright © 2018 Arlen Ricardo Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
