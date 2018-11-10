b7445d75f2c0b2bdaac28b0d3aa7f5d162fb4500
BrunoScheltzke <brunofonten@gmail.com>
2018-11-10 15:06:49 -0200

    adds missing border on statement cell

--- TesteiOSv2/TesteiOSv2/Scenes/StatementList/View/StatementTableViewCell.swift
+++ TesteiOSv2/TesteiOSv2/Scenes/StatementList/View/StatementTableViewCell.swift
@@ -14,10 +14,16 @@ class StatementTableViewCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var roundedView: UIView!
     @IBOutlet weak var shadowView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        roundedView.layer.masksToBounds = true
+        roundedView.layer.cornerRadius = 6
+        roundedView.layer.borderColor = #colorLiteral(red: 0.8582566381, green: 0.8743320107, blue: 0.8909057975, alpha: 0.3).cgColor
+        roundedView.layer.borderWidth = 1
         
         shadowView.layer.addShadow(with: #colorLiteral(red: 0.8498296142, green: 0.8859475255, blue: 0.9151117206, alpha: 1), alpha: 0.5, xOffset: 0, yOffset: 5, blur: 12, spread: 0)
     }
