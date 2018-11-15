3bd4992b125d3fad1d1b0edbafbf1001c9948e24
Roney Sampaio <roney.sampaio@gmail.com>
2018-11-15 15:09:05 -0200

    Global enuns e constants

---
+++ Project/TesteiOSv2/Helper/Global.swift
@@ -0,0 +1,56 @@
+//
+//  Global.swift
+//  CleanCodeLogin
+//
+//  Created by Voll Transportes on 13/11/18.
+//  Copyright © 2018 Roney Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class Global: NSObject {
+    
+    
+    struct Constants {
+        static let accountDetailsStoryboardID = "AccountDetails"
+    }
+    
+    
+    enum RestAPI : String {
+        case baseURL = "https://bank-app-test.herokuapp.com/api/"
+        case requestLogin = "login/"
+        case requestStatements = "statements/"
+    }
+    
+    
+    enum RestParams {
+        
+        enum LoginRequest : String {
+            case kUserAccount = "userAccount"
+            case kAgency = "agency"
+            case kBalance = "balance"
+            case kBankAccount = "bankAccount"
+            case kName = "name"
+            case kUserID = "userId"
+        }
+        
+        enum StatementRequest : String {
+            case kStatementList = "statementList"
+            case kDate = "date"
+            case kDescription = "desc"
+            case kTitle = "title"
+            case kValue = "value"
+        }
+        
+        enum ErrorRequest : String {
+            case kError = "error"
+            case kErrorMsg = "message"
+        }
+        
+    }
+    
+    enum UserDefaults : String {
+        case userID = "userID"
+    }
+    
+}
