1e3afe6861da5825743b37af1ba1de8ed8031032
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-01-08 22:15:08 -0200

    Layout Login

---
+++ Santander/Santander/Model/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Santander
+//
+//  Created by ely.assumpcao.ndiaye on 08/01/19.
+//  Copyright © 2019 ely.assumpcao.ndiaye. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Carregou")
+    }
+
+
+}
+
