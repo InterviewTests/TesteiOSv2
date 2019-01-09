e234bcb393e51b5015f75701768b39c0dd43d55d
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-01-08 22:47:20 -0200

    Libs Alamofire  and SwiftJson and Test API

--- Santander/Santander/Model/LoginVC.swift
+++ Santander/Santander/Model/LoginVC.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Carregou")
+        
+        let parameters = [
+            "user": "test_user",
+            "password": "Test@1"
+        ]
+        
+        AF.request("https://bank-app-test.herokuapp.com/api/login", method: .post, parameters: parameters, encoding:  URLEncoding.httpBody).responseJSON { (response:DataResponse<Any>) in
+            
+            switch(response.result) {
+            case.success(let data):
+                print("success",data)
+            case.failure(let error):
+                print("Not Success",error)
+            }
+            
+        }
+
     }
 
-
+    @IBAction func loginPressed(_ sender: Any) {
+        print("Abriu")
+    }
+    
 }
 
