bc7bedebceed057f3052c224ade955f0d626a198
mariaaeugenia <beds03@gmail.com>
2019-01-10 23:18:08 -0200

    feat: create the project and added gitignore

---
+++ TesteiOSv2-MariaEugenia/TesteiOSv2-MariaEugenia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOSv2-MariaEugenia
+//
+//  Created by Maria Eugênia Teixeira on 10/01/19.
+//  Copyright © 2019 Maria Eugênia Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TesteiOSv2-MariaEugenia/TesteiOSv2-MariaEugeniaTests/TesteiOSv2_MariaEugeniaTests.swift
@@ -0,0 +1,34 @@
+//
+//  TesteiOSv2_MariaEugeniaTests.swift
+//  TesteiOSv2-MariaEugeniaTests
+//
+//  Created by Maria Eugênia Teixeira on 10/01/19.
+//  Copyright © 2019 Maria Eugênia Teixeira. All rights reserved.
+//
+
+import XCTest
+@testable import TesteiOSv2_MariaEugenia
+
+class TesteiOSv2_MariaEugeniaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
