5f54cb6c456e04795e9675c97ba3bf5c96b8eb74
mariaaeugenia <beds03@gmail.com>
2019-01-13 17:33:36 -0200

    feat: created login screen and added logo image

--- TesteiOSv2-MariaEugenia/TesteiOSv2-MariaEugenia/LoginViewController.swift
+++ TesteiOSv2-MariaEugenia/TesteiOSv2-MariaEugenia/LoginViewController.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+    }
+    
 
 }
 
