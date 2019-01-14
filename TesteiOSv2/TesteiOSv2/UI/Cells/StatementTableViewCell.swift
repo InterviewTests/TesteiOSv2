767b08e3c5659b62c4c9364af4a4efcb3c34bc56
Brendoon Ryos <bryos@MBP-de-Brendoon.lan>
2019-01-14 08:39:39 -0200

    Statements layout with localization

--- TesteiOSv2/TesteiOSv2/UI/Cells/StatementTableViewCell.swift
+++ TesteiOSv2/TesteiOSv2/UI/Cells/StatementTableViewCell.swift
@@ -8,17 +8,17 @@
 
 import UIKit
 
-class StatementTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+final class StatementTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var descriptLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var valueLabel: UILabel!
+  
+  func setup(statement: Statement) {
+    titleLabel.text = statement.title
+    descriptLabel.text = statement.description
+    dateLabel.text = Date.stringFormatter(value: statement.date!)
+    valueLabel.text = Double.toCurrency(value: statement.value!)
+  }
 }
