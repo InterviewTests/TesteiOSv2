efe5b0967fc367701c6b39caa01ff21cc509b2a1
Brendoon Ryos <bryos@MBP-de-Brendoon.lan>
2019-01-14 08:44:43 -0200

    Statement Scene

--- TesteiOSv2/TesteiOSv2/Scenes/Statements/StatementsDataSource.swift
+++ TesteiOSv2/TesteiOSv2/Scenes/Statements/StatementsDataSource.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2019 Brendoon Ryos. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StatementsDataSource: NSObject {
+  private let identifier = "StatementTableViewCell"
+  
+  weak var tableView: UITableView? {
+    didSet {
+      let nib = UINib(nibName: identifier, bundle: .main)
+      tableView?.register(nib, forCellReuseIdentifier: identifier)
+    }
+  }
+  
+  var statements: [Statement] = [] {
+    didSet {
+      tableView?.reloadData()
+    }
+  }
+}
+
+extension StatementsDataSource: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return statements.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! StatementTableViewCell
+    cell.setup(statement: statements[indexPath.row])
+    return cell
+  }
+}
