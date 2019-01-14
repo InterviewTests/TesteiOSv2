6b35822454fcc793e584369a585755f6fd7c6bb8
Brendoon Ryos <bryos@MBP-de-Brendoon.lan>
2019-01-14 08:33:21 -0200

    Updating AppIcon

---
+++ TesteiOSv2/TesteiOSv2/Models/Statement.swift
@@ -0,0 +1,9 @@
+//
+//  Statement.swift
+//  TesteiOSv2
+//
+//  Created by Brendoon Ryos on 13/01/19.
+//  Copyright © 2019 Brendoon Ryos. All rights reserved.
+//
+
+import Foundation
---
+++ TesteiOSv2/TesteiOSv2/UI/Cells/StatementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StatementTableViewCell.swift
+//  TesteiOSv2
+//
+//  Created by Brendoon Ryos on 13/01/19.
+//  Copyright © 2019 Brendoon Ryos. All rights reserved.
+//
+
+import UIKit
+
+class StatementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
