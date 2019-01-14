cc225b79d22b3250368193cc93615fae558a7d4c
Brendoon Ryos <bryos@MBP-de-Brendoon.lan>
2019-01-14 08:43:33 -0200

    Statement model

--- TesteiOSv2/TesteiOSv2/Models/Statement.swift
+++ TesteiOSv2/TesteiOSv2/Models/Statement.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+struct Statement {
+  let title: String?
+  let description: String?
+  let date: String?
+  let value: Double?
+}
+
+extension Statement: Decodable {
+  enum StatementCodingKeys: String, CodingKey {
+    case title
+    case description = "desc"
+    case date
+    case value
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: StatementCodingKeys.self)
+    
+    title = try container.decode(String.self, forKey: .title)
+    description = try container.decode(String.self, forKey: .description)
+    date = try container.decode(String.self, forKey: .date)
+    value = try container.decode(Double.self, forKey: .value)
+  }
+  
+}
+
+struct StatementList {
+  let statements: [Statement]?
+  let error: Error?
+}
+
+extension StatementList: Decodable {
+  enum StatementListCodingKeys: String, CodingKey {
+    case statements = "statementList"
+    case error
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: StatementListCodingKeys.self)
+    if container.contains(.statements) {
+      statements = try container.decode([Statement].self, forKey: .statements)
+      error = nil
+    } else {
+      statements = nil
+      error = try container.decode(Error.self, forKey: .error)
+    }
+  }
+}
