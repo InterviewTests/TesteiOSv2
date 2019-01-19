01515deda5d24795d406229c16824713f237638f
Chrystian <salllgado@hotmail.com.br>
2019-01-19 14:13:12 -0200

    #comment Criando caso de teste para userAccount, usando o json de retorno via postman e tentando fazer o parsing do mesmo usando teste

---
+++ Bank App/Bank AppTests/UserAccountParsingTests.swift
@@ -0,0 +1,36 @@
+//
+//  UserAccountParsingTests.swift
+//  Bank AppTests
+//
+//  Created by Chrystian (Pessoal) on 19/01/2019.
+//  Copyright © 2019 Salgado's Productions. All rights reserved.
+//
+
+import XCTest
+@testable import Bank_App
+
+class UserAccountParsingTests: XCTestCase {
+
+    func testDecodable() {
+        let jsonString: [String: Any] = [
+            "userId": 1,
+            "name": "Jose da Silva Teste",
+            "bankAccount": "2050",
+            "agency": "012314564",
+            "balance": 3.3445
+        ]
+        
+        do {
+            let jsonData = try jsonString.convertToJsonData()
+            let objResponse = try JSONDecoder().decode(UserAccount.self, from: jsonData)
+            
+            XCTAssertEqual(objResponse.userId, 1)
+            XCTAssertEqual(objResponse.name, "Jose da Silva Teste")
+            XCTAssertEqual(objResponse.bankAccount, "2050")
+            XCTAssertEqual(objResponse.agency, "01.231456-4")
+            XCTAssertEqual(objResponse.balance, "R$3,34")
+        } catch {
+            XCTFail("\(error.localizedDescription)")
+        }
+    }
+}
