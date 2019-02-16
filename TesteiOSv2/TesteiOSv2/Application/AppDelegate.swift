c7d70b03b37e2f23eb3474d99c1a76d2c2ef6690
Gilson Gil <gilson.gil@heartcare.life>
2019-02-16 02:24:09 -0200

    login scene, resources, layout and offline behaviour

--- TesteiOSv2/TesteiOSv2/Application/AppDelegate.swift
+++ TesteiOSv2/TesteiOSv2/Application/AppDelegate.swift
@@ -14,6 +14,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = LoginViewController()
+    window?.makeKeyAndVisible()
     return true
   }
 }
---
+++ TesteiOSv2/TesteiOSv2/Shared/String+Shared.swift
@@ -0,0 +1,17 @@
+//
+//  String+Shared.swift
+//  TesteiOSv2
+//
+//  Created by Gilson Gil on 16/02/19.
+//  Copyright © 2019 Gilson Gil. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  struct Shared {
+    static var close: String {
+      return "SHARED_CLOSE".localized
+    }
+  }
+}
