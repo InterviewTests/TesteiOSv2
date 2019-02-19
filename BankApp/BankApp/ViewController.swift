e71bc506c0c4127371dcd922e2a56433a26a5e7a
Tripmatix <tripmatix@Tripmatixs-Mac.local>
2019-02-18 18:59:14 -0800

    created project

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Tripmatix on 18/02/19.
+//  Copyright © 2019 Tripmatix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
