e049fda1b9143ad0416646d6c5346d38b94014ca
Lucas Neto Santana <lucas.a.santana@capgemini.com>
2019-02-26 00:53:25 -0300

    Password rules created.

--- TesteiOSv2/Scenes/Login/LoginInteractor.swift
+++ TesteiOSv2/Scenes/Login/LoginInteractor.swift
@@ -32,19 +32,50 @@ class LoginInteractor : LoginBusinessLogic , LoginDataStore{
         self.requestUser(userFields: request.userFormFields)
     }
  
-    private func requestUser(userFields: FetchUsers.UserFormFields) { //-> User {
-        worker?.requestUser(userFields, completionHandler: { (user, error) in
-            if let user = user {
-                self.user = user
-                self.saveUser(userFields.user)
-                self.presenter?.presentFetchUser(response: FetchUsers.FetchUsers.Response(user: self.user))
-            }else{
-                self.presenter?.presentFetchUserError(response: error
-                    ?? "Não foi possivel obter dados do login")
-            }
-        })
+    private func requestUser(userFields: FetchUsers.UserFormFields) {
+        if validPassword(userFields.password) {
+            worker?.requestUser(userFields, completionHandler: { (user, error) in
+                if let user = user {
+                    self.user = user
+                    self.saveUser(userFields.user)
+                    self.presenter?.presentFetchUser(response: FetchUsers.FetchUsers.Response(user: self.user))
+                }else{
+                    self.presenter?.presentFetchUserError(response: error
+                        ?? "Não foi possivel obter dados do login")
+                }
+            })
+        }
     }
-    
+    private func validPassword(_ password:String) -> Bool {
+        let capitalLetterRegEx  = ".*[A-Z]+.*"
+        let texttest = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
+        let capitalresult = texttest.evaluate(with: password)
+        if !capitalresult{
+            presenter?.presentFetchUserError(response: "Senha invalida!\nSenha deve conter ao menos 1 letra maiúscula.")
+            return false
+        }
+        
+        
+        
+        let numberRegEx  = ".*[0-9]+.*"
+        let texttest1 = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
+        let numberresult = texttest1.evaluate(with: password)
+        if !numberresult{
+            presenter?.presentFetchUserError(response: "Senha invalida!\nSenha deve conter ao menos 1 número.")
+            return false
+        }
+        
+        
+        let specialCharacterRegEx  = ".*[!&^%$#@()/]+.*"
+        let texttest2 = NSPredicate(format:"SELF MATCHES %@", specialCharacterRegEx)
+        
+        let specialresult = texttest2.evaluate(with: password)
+        if !specialresult{
+            presenter?.presentFetchUserError(response: "Senha invalida!\nSenha deve conter ao menos 1 caracter especial.")
+            return false
+        }
+        return true
+    }
     private func saveUser(_ login : String){
         BankDAO.save(login)
     }
