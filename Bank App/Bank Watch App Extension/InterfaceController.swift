a4897e4ce2bce9628869d30eb0033e9aefdbc7c5
Chrystian Salgado <salllgado@hotmail.com.br>
2019-03-08 10:29:38 -0300

    Configure bundle identifier for all environments, add add apple watch schemme

---
+++ Bank App/Bank Watch App Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  Bank Watch App Extension
+//
+//  Created by Chrystian Salgado on 08/03/19.
+//  Copyright © 2019 Salgado's Productions. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
