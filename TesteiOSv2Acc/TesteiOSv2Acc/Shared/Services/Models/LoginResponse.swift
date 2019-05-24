5125734f0b5c38e7308337c288dfa4d51d1387d8
Marlon Santos Heitor <marlon.santos.heitor@accenture.com>
2019-05-24 16:26:17 -0300

    WIP - Service layer implementation

---
+++ TesteiOSv2Acc/TesteiOSv2Acc/Shared/Services/Models/LoginResponse.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResponse.swift
+//  TesteiOSv2Acc
+//
+//  Created by Marlon Santos Heitor on 24/05/19.
+//  Copyright © 2019 Marlon Santos Heitor. All rights reserved.
+//
+
+import UIKit
+
+class LoginResponse: NSObject {
+
+}
---
+++ TesteiOSv2Acc/TesteiOSv2Acc/Shared/Services/Rest/RestServiceManager.swift
@@ -0,0 +1,15 @@
+//
+//  RestService.swift
+//  TesteiOSv2Acc
+//
+//  Created by Marlon Santos Heitor on 24/05/19.
+//  Copyright © 2019 Marlon Santos Heitor. All rights reserved.
+//
+
+import Alamofire
+
+class RestService {
+    
+    func 
+    
+}
