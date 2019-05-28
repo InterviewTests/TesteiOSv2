44c11fe9cfffcb2f2f8f3ac022e8dd4fa069b644
Felipe Alexander Silva Melo <felipealexandersilvamelo@SAO-FVFX6821HV29.local>
2019-05-28 15:04:33 -0300

    Created XCode project

---
+++ TesteSantander/TesteSantander/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TesteSantander
+//
+//  Created by Felipe Alexander Silva Melo on 28/05/19.
+//  Copyright © 2019 Felipe Alexander Silva Melo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
