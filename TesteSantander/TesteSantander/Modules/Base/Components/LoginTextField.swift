c77ef0bcd4d2f99230e7dfe63246c8e62adfd6c6
Felipe Alexander Silva Melo <felipealexandersilvamelo@SAO-FVFX6821HV29.local>
2019-05-28 17:18:14 -0300

    Initial login screen

---
+++ TesteSantander/TesteSantander/Modules/Base/Components/LoginTextField.swift
@@ -0,0 +1,26 @@
+//
+//  LoginTextField.swift
+//  TesteSantander
+//
+//  Created by Felipe Alexander Silva Melo on 28/05/19.
+//  Copyright © 2019 Felipe Alexander Silva Melo. All rights reserved.
+//
+
+import UIKit
+
+final class LoginTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setup() {
+        
+    }
+    
+}
