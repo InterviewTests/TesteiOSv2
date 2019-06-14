aef7d316108f11bc16e470769ec17867aefe4998
Fernando Gomes <lfgsnando@gmail.com>
2019-06-14 20:33:51 -0300

    implement login router

--- TesteiOS/TesteiOS/Scenes/Login/LoginRouter.swift
+++ TesteiOS/TesteiOS/Scenes/Login/LoginRouter.swift
@@ -14,7 +14,7 @@ import UIKit
 
 @objc protocol LoginRoutingLogic
 {
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
+  func goToStatement()
 }
 
 protocol LoginDataPassing
@@ -24,37 +24,14 @@ protocol LoginDataPassing
 
 class LoginRouter: NSObject, LoginRoutingLogic, LoginDataPassing
 {
+    
   weak var viewController: LoginViewController?
   var dataStore: LoginDataStore?
-  
-  // MARK: Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
+    
+    func goToStatement() {
+//        let statmentsView = statementsViewController()
+//        statmentsView.modalTransitionStyle = .flipHorizontal
+//        viewController?.present(statmentsView, animated: true, completion: nil)
+    }
 
-  // MARK: Navigation
-  
-  //func navigateToSomewhere(source: LoginViewController, destination: SomewhereViewController)
-  //{
-  //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: Passing data
-  
-  //func passDataToSomewhere(source: LoginDataStore, destination: inout SomewhereDataStore)
-  //{
-  //  destination.name = source.name
-  //}
 }
