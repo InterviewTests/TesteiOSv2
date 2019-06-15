ae4ed1715c5f52fd73ce0032327a772362125ff1
Gabriel Faustino <gabriel.santos@resourceit.com>
2019-06-15 13:13:13 -0300

    Initial Commit

---
+++ BankAppTests/BankAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  BankAppTests.swift
+//  BankAppTests
+//
+//  Created by resource on 15/06/19.
+//  Copyright © 2019 Gabriel Faustino. All rights reserved.
+//
+
+import XCTest
+@testable import BankApp
+
+class BankAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
