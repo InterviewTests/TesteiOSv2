be6cf5a448406f807f95a7904bf1d432296b8f0e
Fernando Gomes <lfgsnando@gmail.com>
2019-06-15 18:36:02 -0300

    add table view for statements
    create header for table view statements

---
+++ TesteiOS/TesteiOS/Scenes/Login/StatementTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StatementTableViewCell.swift
+//  TesteiOS
+//
+//  Created by Fernando Gomes on 15/06/19.
+//  Copyright © 2019 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class StatementTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+}
