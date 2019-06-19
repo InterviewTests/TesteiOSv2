fc9ef1fc7671ae0ae670534cd2b9c78d47a033ee
Lucas Ferracioli Assis <lucas.assis-ext@ab-inbev.com>
2019-06-19 11:03:17 -0300

    Account unit tests

--- Santander Teste iOS/Santander Teste iOS Tests/accountScene/AccountSpec.swift
+++ Santander Teste iOS/Santander Teste iOS Tests/accountScene/AccountSpec.swift
@@ -32,6 +32,14 @@ class AccountSpec: QuickSpec {
                 interactor.worker = worker
                 presenter.viewController = viewController
                 router.viewController = viewController
+                UIApplication.shared.keyWindow?.rootViewController = viewController
+                viewController.loadViewIfNeeded()
+            }
+            
+            afterEach {
+                if viewController.presentedViewController != nil {
+                    viewController.dismiss(animated: false, completion: nil)
+                }
             }
             
             context("VIP setup", {
@@ -62,8 +70,40 @@ class AccountSpec: QuickSpec {
                 })
             })
             
-            context("", closure: {
+            context("User account data", closure: {
+                it("Set user account", closure: {
+                    let userAccount = UserAccount(userId: 0, name: "Lucas Conte Ferracioli", bankAccount: "1234", agency: "123456789", balance: 1000.0)
+                    interactor.setUserAccount(userAccount: userAccount)
+                    expect(interactor.userAccount).toNot(beNil())
+                })
+            })
+            
+            context("Statements data", closure: {
+                it("Fetch data", closure: {
+                    interactor.fetchStatementList()
+                    expect(interactor.getStatementList()).toEventuallyNot(beNil(), timeout: 3.0, pollInterval: 0.2, description: nil)
+                })
+            })
+            
+            context("Logout", closure: {
+                it("Display logout alert", closure: {
+                    interactor.logout()
+                    expect(viewController.presentedViewController).toEventually(beAKindOf( UIAlertController.self), timeout: 3.0, pollInterval: 0.2, description: nil)
+                })
+            })
+            
+            context("Presenter validation", closure: {
+                it("Currency mask", closure: {
+                    expect(presenter.parseSaldoToString(saldo: 10.0)).to(equal("R$ 10.00"))
+                })
                 
+                it("Agency mask", closure: {
+                    expect(presenter.formatAgency(agency: "123456789")).to(equal("12.345678-9"))
+                })
+                
+                it("Agency and account mask", closure: {
+                    expect(presenter.formatAgencyBankAccount(agency: "123456789", bankAccount: "1234")).to(equal("1234 / 12.345678-9"))
+                })
             })
         }
     }
