a0a34a7fbda7421b63992d6953fa9fc0a62b9f19
Thiago Santos <t.vaz.santos@gmail.com>
2019-07-11 18:21:09 -0300

    .gitignore funcionando ;)

--- teste_santander/Pods/Target Support Files/Pods-Teste Santander/Pods-Teste Santander-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Teste_Santander : NSObject
-@end
-@implementation PodsDummy_Pods_Teste_Santander
-@end
--- teste_santander/Pods/Target Support Files/RNLoadingButton-Swift/RNLoadingButton-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RNLoadingButton_Swift : NSObject
-@end
-@implementation PodsDummy_RNLoadingButton_Swift
-@end
