59fcaa4c19e5be2e2dc858e7df25d3ba76c303be
Thiago Santos <t.vaz.santos@gmail.com>
2019-07-11 21:06:38 -0300

    [FIX] Ajuste no teste homeviewcontroller

--- teste_santander/Teste SantanderTests/Home/HomeViewControllerTeste.swift
+++ teste_santander/Teste SantanderTests/Home/HomeViewControllerTeste.swift
@@ -12,7 +12,7 @@ import Nimble
 @testable import Teste_Santander
 
 class HomeViewControllerTeste: QuickSpec {
-
+    
     override func spec() {
         let viewController: HomeViewController = HomeWireframe().makeScreen(window: nil)
         
@@ -22,6 +22,7 @@ class HomeViewControllerTeste: QuickSpec {
         
         describe("HomeViewControllerTest") {
             context("Verify sections") {
+                
                 it("Testing count Sections") {
                     expect(viewController.sections.count).toEventually(equal(10), timeout: 2.0)
                 }
@@ -36,6 +37,23 @@ class HomeViewControllerTeste: QuickSpec {
                     expect(section.item.name).toEventually(equal("Jose da Silva Teste"), timeout: 2.0)
                     expect(section.item.userID).toEventually(equal(1), timeout: 2.0)
                     expect(section.item.balance).toEventually(equal(3.3445), timeout: 2.0)
+                }
+                
+                it("Testing Cell User") {
+                    
+                    guard let section = viewController.sections.first as? SectionHeader else {
+                        fail("Section not found")
+                        return
+                    }
+                    let indexPath = IndexPath(item: 0, section: 0)
+                    guard let cell = section.cell(for: indexPath).fromNib() as? HeaderCellView else {
+                        fail("Cell not found")
+                        return
+                    }
+                    section.willDisplayCell(cell, at: indexPath)
+                    expect(cell.nameLabel.text).toEventually(contain("Jose da Silva Teste"), timeout: 2.0)
+                    expect(cell.bankAccountLabel.text).toEventually(contain("2050 / 01.231456-4"), timeout: 2.0)
+                    expect(cell.balanceLabel.text).toEventually(contain("R$ 3,34"), timeout: 2.1)
                 }
             }
         }
