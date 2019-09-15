86aa25904ffd9ea66bb65e71236aacffdfcf6b8d
Leonardo Coutinho Santos <lcoutinh@SAO-C02V1GHKHV22.local>
2019-09-15 20:57:39 -0300

    added project files and gitignore

---
+++ Bank-app/Bank-appTests/Bank_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bank_appTests.swift
+//  Bank-appTests
+//
+//  Created by Leonardo Coutinho Santos on 15/09/19.
+//  Copyright © 2019 Leocoout.dev. All rights reserved.
+//
+
+import XCTest
+@testable import Bank_app
+
+class Bank_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
