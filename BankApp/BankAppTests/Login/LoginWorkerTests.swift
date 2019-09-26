98f183c9857bf6a206226386b5d20cc85c0ffcc1
Marcus Titton <mtitton@ntb139005.corp.capgemini.com>
2019-09-26 12:02:13 -0300

    Inclusão de novos testes unitários

--- BankApp/BankAppTests/Login/LoginWorkerTests.swift
+++ BankApp/BankAppTests/Login/LoginWorkerTests.swift
@@ -19,8 +19,8 @@ class LoginWorkerTests: XCTestCase {
         super.tearDown()
     }
 
-    //Função que testa o response do worker/service
-    func testCompletionHandler() {
+    //Função que testa o response do worker/service FUNÇÃO MOCKADO
+    func testCompletionHandlerMock() {
         //Inicializa o worker com base no LoginService (onde utilizamos o protocol para fazer a chamada na API)
         var worker: LoginWorker?
         worker = LoginWorker(LoginService())
@@ -39,6 +39,29 @@ class LoginWorkerTests: XCTestCase {
                       "É o retorno que está configurado para o Agency na função mockLogin do LoginService")
             XCTAssert(response?.balance == 200,
                       "É o retorno que está configurado para o Balance na função mockLogin do LoginService")
+        }
+    }
+    
+    //Função que testa o response do worker/service
+    func testCompletionHandler() {
+        //Inicializa o worker com base no LoginService (onde utilizamos o protocol para fazer a chamada na API)
+        var worker: LoginWorker?
+        worker = LoginWorker(LoginService())
+        
+        //Chama a função de login (com os dados de acesso) e configura o response como varivel de retorno
+        worker?.login("teste@teste.com.br", password: "Teste1@") { (response: User?) in
+            
+            XCTAssertNotNil(response)
+            XCTAssert(response?.userId == 1,
+                      "É o retorno que está configurado para o UserId na função login do LoginService")
+            XCTAssert(response?.name == "Jose da Silva Teste",
+                      "É o retorno que está configurado para o Name na função login do LoginService")
+            XCTAssert(response?.bankAccount == "2050",
+                      "É o retorno que está configurado para o BankAccount na função login do LoginService")
+            XCTAssert(response?.agency == "012314564",
+                      "É o retorno que está configurado para o Agency na função login do LoginService")
+            XCTAssert(response?.balance == 3.3445,
+                      "É o retorno que está configurado para o Balance na função login do LoginService")
         }
     }
     
--- BankApp/BankAppTests/Statement/StatementWorkerTests.swift
+++ BankApp/BankAppTests/Statement/StatementWorkerTests.swift
@@ -21,8 +21,8 @@ class StatementWorkerTests: XCTestCase {
         super.tearDown()
     }
 
-    //Função que testa o response do worker/service
-    func testCompletionHandler() {
+    //Função que testa o response do worker/service - Mockado
+    func testCompletionHandlerMock() {
         //Inicializa o worker com base no StatementService (onde utilizamos o protocol para fazer a chamada na API)
         var worker: StatementWorker?
         worker = StatementWorker(StatementService())
@@ -39,6 +39,27 @@ class StatementWorkerTests: XCTestCase {
                         "É o retorno que está configurado para o date na função mockStatment do StatementService")
             XCTAssert(response[0]?.value == 123,
                         "É o retorno que está configurado para o value na função mockStatment do StatementService")
+        }
+    }
+    
+    //Função que testa o response do worker/service
+    func testCompletionHandler() {
+        //Inicializa o worker com base no StatementService (onde utilizamos o protocol para fazer a chamada na API)
+        var worker: StatementWorker?
+        worker = StatementWorker(StatementService())
+        
+        //Chama a função de extrato (com o id do usuario logado) e configura o response como varivel de retorno
+        worker?.getStatement(1) { (response: [StatementUser?]) in
+            XCTAssertNotNil(response)
+            
+            XCTAssert(response[0]?.title == "Pagamento",
+                      "É o retorno que está configurado para o title na função getStatement do StatementService")
+            XCTAssert(response[0]?.desc == "Conta de luz",
+                      "É o retorno que está configurado para o desc na função getStatement do StatementService")
+            XCTAssert(response[0]?.date == "2018-08-15",
+                      "É o retorno que está configurado para o date na função getStatement do StatementService")
+            XCTAssert(response[0]?.value == -50,
+                      "É o retorno que está configurado para o value na função getStatement do StatementService")
         }
     }
 }
