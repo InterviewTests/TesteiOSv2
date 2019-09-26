357cd13e91e7f84cff72bf1555a2f22131c0e03a
Marcus Titton <mtitton@ntb139005.corp.capgemini.com>
2019-09-26 09:56:01 -0300

    Inclusão de novos testes unitários

--- BankApp/BankAppTests/StatementPresenterTests.swift
+++ BankApp/BankAppTests/StatementPresenterTests.swift
@@ -1,5 +1,5 @@
 //
-//  LoginPresenterTests.swift
+//  StatementPresenterTests.swift
 //  BankAppTests
 //
 //  Created by Marcus Titton on 26/09/19.
@@ -9,15 +9,15 @@
 import XCTest
 @testable import BankApp
 
-class MockPresenterOutput: LoginDisplayLogic {
+class MockPresenterStatement: StatementDisplayLogic {
     var displayCalled = false
     
-    func showStatement() {
+    func showStatement(statements: Statement.StatementApi.Response) {
         displayCalled = true
     }
 }
 
-class LoginPresenterTests: XCTestCase {
+class StatementPresenterTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -31,11 +31,17 @@ class LoginPresenterTests: XCTestCase {
     
     //Teste que verifica se o presenter que chama a tela de extrato está sendo chamado
     func testCallingOutput() {
-        let presenter = LoginPresenter()
-        let output = MockPresenterOutput()
+        let presenter = StatementPresenter()
+        let output = MockPresenterStatement()
+        
+        let statementTest1 = StatementUser.init(title: "Teste 1", desc: "Teste 1", date: "Teste 1", value: 01.2)
+        let statementTest2 = StatementUser.init(title: "Teste 2", desc: "Teste 2", date: "Teste 2", value: 02.3)
+        
+        let statements = [statementTest1, statementTest2]
+        let response = Statement.StatementApi.Response(statement: statements)
         
         presenter.viewController = output
-        presenter.presentLogin()
+        presenter.presentStatement(response)
         
         XCTAssert(output.displayCalled,
                   "Deve chamar a função que exibe o present")
---
+++ BankApp/BankAppTests/StatementWorkerTests.swift
@@ -0,0 +1,44 @@
+//
+//  StatementWorkerTests.swift
+//  BankAppTests
+//
+//  Created by Marcus Titton on 26/09/19.
+//  Copyright © 2019 Marcus Titton. All rights reserved.
+//
+
+import XCTest
+@testable import BankApp
+
+class StatementWorkerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    //Função que testa o response do worker/service
+    func testCompletionHandler() {
+        //Inicializa o worker com base no StatementService (onde utilizamos o protocol para fazer a chamada na API)
+        var worker: StatementWorker?
+        worker = StatementWorker(StatementService())
+        
+        //Chama a função de extrato (com o id do usuario logado) e configura o response como varivel de retorno
+        worker?.mockStatement(1) { (response: [StatementUser?]) in
+            XCTAssertNotNil(response)
+            
+            XCTAssert(response[0]?.title == "Pagamento",
+                        "É o retorno que está configurado para o title na função mockStatment do StatementService")
+            XCTAssert(response[0]?.desc == "Descrição",
+                        "É o retorno que está configurado para o desc na função mockStatment do StatementService")
+            XCTAssert(response[0]?.date == "26/09/2019",
+                        "É o retorno que está configurado para o date na função mockStatment do StatementService")
+            XCTAssert(response[0]?.value == 123,
+                        "É o retorno que está configurado para o value na função mockStatment do StatementService")
+        }
+    }
+}
