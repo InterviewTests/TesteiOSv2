1680226889d3ea6f046da7d2fc42ac8b9600cb29
Arlen2014 <arlen.r.pereira@gmail.com>
2020-01-28 08:09:23 -0300

    Was created the Login and Statement views.

---
+++ TesteiOSv2_ArlenPereira/TesteiOSv2_ArlenPereira/Scenes/Statement/StatementCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  StatementCollectionViewCell.swift
+//  TesteiOSv2_ArlenPereira
+//
+//  Created by Arlen Ricardo Pereira on 28/01/20.
+//  Copyright © 2020 Arlen Ricardo Pereira. All rights reserved.
+//
+
+import UIKit
+
+class StatementCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
