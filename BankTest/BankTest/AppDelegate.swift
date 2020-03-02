45e17c7ad31dcad71c162d4c1d5af7f1b70b748a
Luan Almeida <luan.nascimento.almeida@everis.com>
2020-03-02 09:54:12 -0300

    Configuracao base
    Primeiro commit com a configuracao base do projeto

---
+++ BankTest/BankTest/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  BankTest
+//
+//  Created by Luan Nascimento Almeida on 02/03/20.
+//  Copyright © 2020 bank. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
