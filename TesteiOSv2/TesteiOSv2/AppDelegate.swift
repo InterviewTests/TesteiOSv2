cc031cde0503a7330ddf246a6df0833d6c061236
Mônica Marques <mo.menezes.marques@gmail.com>
2020-03-10 20:43:22 -0300

    first commit with loginModule and layout

---
+++ TesteiOSv2/TesteiOSv2/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TesteiOSv2
+//
+//  Created by Mônica Marques on 10/03/20.
+//  Copyright © 2020 Mônica Marques. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TesteiOSv2/TesteiOSv2Tests/TesteiOSv2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TesteiOSv2Tests.swift
+//  TesteiOSv2Tests
+//
+//  Created by Mônica Marques on 10/03/20.
+//  Copyright © 2020 Mônica Marques. All rights reserved.
+//
+
+import XCTest
+@testable import TesteiOSv2
+
+class TesteiOSv2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
