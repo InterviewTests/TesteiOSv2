55893ad791f274d3bdda10bef7a495d61494eabd
Osias Carneiro <osias.carneiro@solutis.com.br>
2020-03-11 16:22:08 -0300

    Formatando data

--- BankInterviewApp/BankInterviewApp/Scenes/Statements/StatementsInteractor.swift
+++ BankInterviewApp/BankInterviewApp/Scenes/Statements/StatementsInteractor.swift
@@ -29,15 +29,32 @@ class StatementsInteractor: StatementsBusinessLogic, StatementsDataStore
   var statementsWorker: FetchStatementsWorker = FetchStatementsWorker(store: StatementAPI())
   var userData: Statements.UserData.ViewModel?
   
-  // MARK: Do something
+    let dateFormatterFrom: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    let dateFormatterTo: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        return formatter
+    }()
+    
+  // MARK: Fetch statements
   
   func fetchStatements(request: Statements.StatementList.Request)
   {
     statementsWorker.fetchStatements(userId: request.userId, completionHandler: { [weak self] response in
         if !response.statementList.isEmpty {
             var model = Statements.StatementList.ViewModel(statements: [])
-            let transformed = response.statementList.map({
-                Statements.StatementList.Statement(transaction: $0.title, description: $0.desc, date: $0.date, value: $0.value.formatToCoin())
+            let transformed = response.statementList.map({ (item) -> Statements.StatementList.Statement in
+                var dateFormatted = ""
+                if let date = self?.dateFormatterFrom.date(from: item.date) {
+                    dateFormatted = self?.dateFormatterTo.string(from: date) ?? ""
+                }
+                
+                return Statements.StatementList.Statement(transaction: item.title, description: item.desc, date: dateFormatted, value: item.value.formatToCoin())
             })
             model.statements.append(contentsOf: transformed)
             self?.presenter?.presentFetchedStatements(response: model)
