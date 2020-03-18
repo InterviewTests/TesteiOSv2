e576d651297e64eecbbd012ca921bb01685b0ac9
Bárbara Aniele <barbara.jesus@solutis.com.br>
2020-03-18 15:09:48 -0300

    - Criação do projeto - Definindo target para iOS 9.3

---
+++ Bank_App/Bank_App/Bank_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank_App
+//
+//  Created by apple on 18/03/20.
+//  Copyright © 2020 Barbara_Aniele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ Bank_App/Bank_App/Bank_AppTests/Bank_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bank_AppTests.swift
+//  Bank_AppTests
+//
+//  Created by apple on 18/03/20.
+//  Copyright © 2020 Barbara_Aniele. All rights reserved.
+//
+
+import XCTest
+@testable import Bank_App
+
+class Bank_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
