973d0ae406104c6f3baef6769e76192915650cbf
Maíra Preto <mairapreto@me.com>
2020-04-19 15:22:56 -0300

    Teste unitário das xibs. Utilizei XCTest, por ser nativo.

---
+++ SANTANDER-ACCENTURE/SANTANDER/SANTANDERTests/View/ViewCell/LancamentosUsuarioTableViewCellTests.swift
@@ -0,0 +1,39 @@
+//
+//  LancamentosUsuarioTableViewCellTests.swift
+//  SANTANDERTests
+//
+//  Created by Maíra Preto on 19/04/20.
+//  Copyright © 2020 Maíra Acioli. All rights reserved.
+//
+
+import XCTest
+@testable import SANTANDER
+
+class LancamentosUsuarioTableViewCellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   func test_setCell(){
+        let bundle = Bundle(for: LancamentosUsuarioTableViewCell.self)
+        guard let cell = bundle.loadNibNamed("LancamentosUsuarioTableViewCell", owner: nil)?.first as? LancamentosUsuarioTableViewCell else {
+            return XCTFail()
+        }
+        
+        let statement = StatementList(title: "TED", desc: "Aluguel", date: "2020/04/20", value: 5025.00)
+
+    cell.set(list: statement)
+        
+        XCTAssert(cell.tituloLabel.text == "TED")
+        XCTAssert(cell.descricaoLabel.text == "Aluguel")
+        XCTAssert(cell.dataLabel.text == "20/04/2020")
+        XCTAssert(cell.valorLabel.text == "R$ 5025.00")
+    }
+
+
+}
---
+++ SANTANDER-ACCENTURE/SANTANDER/SANTANDERTests/View/ViewCell/LoginTableViewCellTests.swift
@@ -0,0 +1,64 @@
+//
+//  LoginTableViewCellTests.swift
+//  SANTANDERTests
+//
+//  Created by Maíra Preto on 19/04/20.
+//  Copyright © 2020 Maíra Acioli. All rights reserved.
+//
+
+import XCTest
+@testable import SANTANDER
+
+class LoginTableViewCellTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+ func test_verifyValidFields(){
+            let bundle = Bundle(for: LoginTableViewCell.self)
+            guard let cell = bundle.loadNibNamed("LoginTableViewCell", owner: nil)?.first as? LoginTableViewCell else {
+                return XCTFail()
+            }
+            cell.nomeUsuarioTextField.text = "mairapreto@me.com"
+            cell.senhaUsuarioTextField.text = "aS1@"
+            let valid = cell.verifyIfValidFields()
+            
+            XCTAssertEqual(valid, true)
+        }
+        
+        func test_setupDefaultValuesOnDismiss(){
+            let bundle = Bundle(for: LoginTableViewCell.self)
+            guard let cell = bundle.loadNibNamed("LoginTableViewCell", owner: nil)?.first as? LoginTableViewCell else {
+                return XCTFail()
+            }
+            cell.setupDefaultValuesOnDismiss()
+            XCTAssertEqual(cell.nomeUsuarioTextField.placeholder, "User")
+            XCTAssertEqual(cell.senhaUsuarioTextField.placeholder, "Password")
+            XCTAssertEqual(cell.senhaUsuarioTextField.text, "")
+           
+        }
+    
+
+        
+        func test_loginButton(){
+            let bundle = Bundle(for: LoginTableViewCell.self)
+            guard let cell = bundle.loadNibNamed("LoginTableViewCell", owner: nil)?.first as? LoginTableViewCell else {
+                return XCTFail()
+            }
+            cell.nomeUsuarioTextField.text = "MairaAcioli"
+            cell.senhaUsuarioTextField.text = "aS2@"
+            
+            cell.loginButton(AnyObject.self)
+            XCTAssertEqual(cell.nomeUsuarioTextField.layer.borderColor, UIColor.red.cgColor)
+            XCTAssertEqual(cell.senhaUsuarioTextField.layer.borderColor, UIColor.red.cgColor)
+        }
+    
+    }
+
+
+
