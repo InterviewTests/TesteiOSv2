97e4c1a6d50a27268cf65108171e58161f6b09e7
Matheus Ribeiro <empresas.mr.two@gmail.com>
2020-04-27 05:36:21 -0300

    Testes unitários de login

---
+++ TataSantanderBankTest/TataSantanderBankTestTests/Mocks/UserServiceMock.swift
@@ -0,0 +1,34 @@
+//
+//  UserServiceMock.swift
+//  Bank
+//
+//  Created by Matheus Rodrigues Ribeiro on 26/04/20.
+//  Copyright © 2020 Bank. All rights reserved.
+//
+
+import Foundation
+@testable import Bank
+
+class UserServiceMock: UserStoreProtocol {
+    
+    let user = "test_user@mail.com"
+    let password = "Test@1"
+    var localUser: String?
+    
+    func login(user: String, password: String, completionHandler: @escaping (LoginResponse) -> ()) {
+        if(user == self.user && password == self.password) {
+            completionHandler(LoginResponse(userAccount: User(userId: 1, name: "Matheus Rodrigues Ribeiro", bankAccount: "0001", agency: "000012", balance: 415.57), error: Error()))
+        } else {
+            completionHandler(LoginResponse(userAccount: User(), error: Error(message: "Usuário ou senha inválidos", code: 500)))
+        }
+    }
+    
+    func fetchUser() -> String? {
+        return self.localUser
+    }
+    
+    func storeUser(user: String) {
+        self.localUser = user
+    }
+    
+}
