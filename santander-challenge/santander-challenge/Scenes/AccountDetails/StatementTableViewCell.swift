01b8038c3ee25c7de0452a03e60860a9d44961dc
Kevin Oliveira <kevinobruno@gmail.com>
2020-05-03 19:01:38 -0400

    feat: Cria scene de AccountDetails
    Essa scene foi criada utilizando o template da arquitetura
    Clean Swift.

---
+++ santander-challenge/santander-challenge/Scenes/AccountDetails/StatementTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StatementTableViewCell.swift
+//  santander-challenge
+//
+//  Created by Kevin Oliveira on 02/05/20.
+//  Copyright © 2020 Kevin Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class StatementTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+}
