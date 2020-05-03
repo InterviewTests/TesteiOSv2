ccb43927706c92271305ae6025d6708e19aaf07f
Kevin Oliveira <kevinobruno@gmail.com>
2020-05-03 18:40:51 -0400

    chore: Cria projeto inicial

---
+++ santander-challenge/santander-challenge/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  santander-challenge
+//
+//  Created by Kevin Oliveira on 01/05/20.
+//  Copyright © 2020 Kevin Oliveira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
