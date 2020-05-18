f9c37bbbac278af8a0a8388e173d9a2f8ad73726
mmalaqui <mmalaqui@everis.com>
2020-05-18 09:42:21 +0100

    added base project (xcode init)

---
+++ project/Bank app/Bank app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bank app
+//
+//  Created by mmalaqui on 18/05/2020.
+//  Copyright © 2020 everis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
