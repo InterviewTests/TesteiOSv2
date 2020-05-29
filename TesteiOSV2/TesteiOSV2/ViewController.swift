5966c32ae079b22c842188699ef00fa4554f4120
Julio Cezar Souza <julio.souza@gmail.com>
2020-05-28 21:16:23 -0300

    initial project commit

---
+++ TesteiOSV2/TesteiOSV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesteiOSV2
+//
+//  Created by Julio Cezar de Souza on 28/05/20.
+//  Copyright © 2020 Julio Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
