088ea0c1630f84c74460cdfbb9aa36a228fa83ff
Estaife Lima <estaife@icloud.com>
2020-05-30 11:40:37 -0400

    Created Domain layer

---
+++ BankApp/Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Estaife Lima on 30/05/20.
+//  Copyright © 2020 Estaife Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
