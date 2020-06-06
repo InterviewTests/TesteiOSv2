d74b706f59a0abf1da8c453738fa5a6ae474b3b9
Estaife Lima <estaifeoliveira@ioasys.com.br>
2020-06-06 11:49:25 -0400

    Added Validator layer to valid CPF and e-mail

---
+++ BankApp/Validator/Validator.h
@@ -0,0 +1,19 @@
+//
+//  Validator.h
+//  Validator
+//
+//  Created by Estaife Lima on 06/06/20.
+//  Copyright © 2020 Estaife Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Validator.
+FOUNDATION_EXPORT double ValidatorVersionNumber;
+
+//! Project version string for Validator.
+FOUNDATION_EXPORT const unsigned char ValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validator/PublicHeader.h>
+
+
---
+++ BankApp/ValidatorTests/UserNameValidator.swift
@@ -0,0 +1,21 @@
+//
+//  UserNameValidator.swift
+//  ValidatorTests
+//
+//  Created by Estaife Lima on 06/06/20.
+//  Copyright © 2020 Estaife Lima. All rights reserved.
+//
+
+import XCTest
+@testable import Validator
+import Presenter
+
+class UserNameValidator: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+
+    
+
+}
