532f5b8a58b1da1f383f97359f3c0290839ec65c
Estaife Lima <estaifeoliveira@ioasys.com.br>
2020-06-05 22:38:08 -0400

    Created UserInteraction layer and allow build in mac and iphonesimulator and iphoneos

---
+++ BankApp/UserInterface/UserInterface.h
@@ -0,0 +1,19 @@
+//
+//  UserInterface.h
+//  UserInterface
+//
+//  Created by Estaife Lima on 05/06/20.
+//  Copyright © 2020 Estaife Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserInterface.
+FOUNDATION_EXPORT double UserInterfaceVersionNumber;
+
+//! Project version string for UserInterface.
+FOUNDATION_EXPORT const unsigned char UserInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserInterface/PublicHeader.h>
+
+
---
+++ BankApp/UserInterfaceTests/Login/LoginViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterfaceTests.swift
+//  UserInterfaceTests
+//
+//  Created by Estaife Lima on 05/06/20.
+//  Copyright © 2020 Estaife Lima. All rights reserved.
+//
+
+import XCTest
+@testable import UserInterface
+
+class LoginViewControllerTests: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+}
