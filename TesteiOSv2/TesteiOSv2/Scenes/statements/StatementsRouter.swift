9f015760a158a87bd91590c899cfaa72958a885b
Foliveira <foliveira@Foliveiras-Mac.local>
2020-06-14 14:47:16 -0700

    Finalizado

---
+++ TesteiOSv2/TesteiOSv2/Scenes/statements/StatementsRouter.swift
@@ -0,0 +1,9 @@
+//
+//  StatementsRouter.swift
+//  TesteiOSv2
+//
+//  Created by Foliveira on 13/06/20.
+//  Copyright © 2020 Foliveira. All rights reserved.
+//
+
+import Foundation
---
+++ TesteiOSv2/TesteiOSv2/Utils/Extensions/StringExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  StringExtensions.swift
+//  TesteiOSv2
+//
+//  Created by Foliveira on 14/06/20.
+//  Copyright © 2020 Foliveira. All rights reserved.
+//
+
+import Foundation
