9556ed3966b204f11f77021c0d5265696d43b066
Lucas <lucasbrito_9@hotmail.com>
2020-06-20 17:12:03 -0300

    Init project.

---
+++ Test Bank/Test Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Bank
+//
+//  Created by Lucas Santana Brito on 20/06/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test Bank/Test BankTests/Test_BankTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_BankTests.swift
+//  Test BankTests
+//
+//  Created by Lucas Santana Brito on 20/06/20.
+//  Copyright © 2020 lsb. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Bank
+
+class Test_BankTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
