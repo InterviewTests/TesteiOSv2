adc772ed82fe2b3321c143139a623b4725f90ce5
Rafael Valer <rafael@aegro.com.br>
2020-07-20 16:04:25 -0300

    fix unnecessary files

--- TCS_Challenge/Pods/Target Support Files/Pods-TCS_Challenge-TCS_ChallengeUITests/Pods-TCS_Challenge-TCS_ChallengeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TCS_Challenge_TCS_ChallengeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TCS_Challenge_TCS_ChallengeUITests
-@end
--- TCS_Challenge/Pods/Target Support Files/Pods-TCS_ChallengeTests/Pods-TCS_ChallengeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TCS_ChallengeTests : NSObject
-@end
-@implementation PodsDummy_Pods_TCS_ChallengeTests
-@end
