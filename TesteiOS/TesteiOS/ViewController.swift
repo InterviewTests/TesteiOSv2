ddbe27d6e53cc564b6b44d042ac6fc0687fd76cf
leoramoxx <leandroaaramos@gmail.com>
2020-07-25 09:00:36 -0300

    login screen layouted

---
+++ TesteiOS/TesteiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testeios
+//
+//  Created by VM on 24/07/20.
+//  Copyright © 2020 VM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
