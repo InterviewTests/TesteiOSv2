dd1c904fcf2fe813374401478b8cba63226901e2
Lucas Fernandes Araujo <luk.fa@hotmail.com>
2020-07-30 19:48:22 -0300

    criação do projeto no xcode

---
+++ Teste_iOS_v2/Teste_iOS_v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teste_iOS_v2
+//
+//  Created by Lucas Fernandes Araujo on 30/07/20.
+//  Copyright © 2020 Lucas Fernandes Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Teste_iOS_v2/Teste_iOS_v2Tests/Teste_iOS_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Teste_iOS_v2Tests.swift
+//  Teste_iOS_v2Tests
+//
+//  Created by Lucas Fernandes Araujo on 30/07/20.
+//  Copyright © 2020 Lucas Fernandes Araujo. All rights reserved.
+//
+
+import XCTest
+@testable import Teste_iOS_v2
+
+class Teste_iOS_v2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
