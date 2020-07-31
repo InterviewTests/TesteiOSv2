1a10472c85e5b10a40c9ecf657a40daf6f0c8604
Lucas Fernandes Araujo <luk.fa@hotmail.com>
2020-07-30 23:34:26 -0300

    adição de NibLoadable para instanciação de UIViews, e alteração no AppDelegate e deleção do SceneDelegate para dar suporte ao iOS 10+

---
+++ Teste_iOS_v2/Teste_iOS_v2/Infrastructure/NibLoadable/NibLoadable.swift
@@ -0,0 +1,55 @@
+//
+//  NibLoadable.swift
+//  Teste_iOS_v2
+//
+//  Created by Lucas Fernandes Araujo on 30/07/20.
+//  Copyright © 2020 Lucas Fernandes Araujo. All rights reserved.
+//
+
+import UIKit
+
+public protocol NibLoadable {
+    static var nibName: String { get }
+}
+
+public extension NibLoadable where Self: UIView {
+    
+    static var nibName: String {
+        return String(describing: Self.self) // defaults to the name of the class implementing this protocol.
+    }
+    
+    static var nib: UINib {
+        let bundle = Bundle(for: Self.self)
+        return UINib(nibName: Self.nibName, bundle: bundle)
+    }
+    
+    func setupFromNib() {
+        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else { fatalError("Error loading \(self) from nib") }
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 11.0, *) {
+            
+            view.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            view.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            view.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+            view.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        } else {
+            view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+            view.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+            view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        }
+    }
+    
+    func setupFromNibFullScreen() {
+        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Error loading \(self) from nib")
+        }
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        view.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+    }
+}
