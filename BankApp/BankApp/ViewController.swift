a4c72a0e9ee88d39f6b2e0e8be3386a35f02d836
brmaciel <44143089+brmaciel@users.noreply.github.com>
2021-01-16 10:57:17 -0300

    [NEW] Init project

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Bruno Maciel on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
