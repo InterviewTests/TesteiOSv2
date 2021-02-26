6ff6efc5550a6a16efb0a25e1654792baeb5dfc1
Mízia Lima <mizia.alima@gmail.com>
2021-02-26 13:38:13 -0300

    Cria elementos da view de login

--- BankApp-Desafio/BankApp-Desafio/SceneDelegate.swift
+++ BankApp-Desafio/BankApp-Desafio/SceneDelegate.swift
@@ -15,7 +15,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: <#T##UIWindowScene#>)
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = LoginViewController(nibName: nil, bundle: nil)
+        
+        self.window = window
+        
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
