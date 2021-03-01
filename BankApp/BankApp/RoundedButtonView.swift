169e895544cf64c99e268aca423e4c3e3b290d15
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2021-03-01 16:10:38 -0300

    Put elements in login screen

---
+++ BankApp/BankApp/RoundedButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedButtonView.swift
+//  BankApp
+//
+//  Created by Adriano Rodrigues Vieira on 01/03/21.
+//
+
+import UIKit
+
+class RoundedButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
