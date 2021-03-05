6a7ccc1a523be16ca98c61967e746458b917bc52
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-03-05 18:34:31 -0300

    Update README.md

--- BankApp/BankApp/Model/ViewModel/UserAccountViewModel.swift
+++ BankApp/BankApp/Model/ViewModel/UserAccountViewModel.swift
@@ -14,14 +14,18 @@ struct UserAccountViewModel {
 
     init(from userAccount: UserAccount) {
         self.userName = userAccount.name
-        self.accountWithAgency = Self.createFormattedAccountWithAgencyString(account: userAccount.bankAccount, agency: userAccount.agency)
-        self.amount = CurrencyConverter.convertUSNumberValueToBRStringValue(userAccount.balance)
+        
+        // Aqui tive que inverter a ordem dos parametros, pois a API está com os dados invertidos (a agencia eh o numero menor,
+        // e a conta o numero maior)
+        let switchedBankAccount = userAccount.agency
+        let switchedAgency = userAccount.bankAccount
+        
+        self.accountWithAgency = Self.createFormattedAccountWithAgencyString(account: switchedBankAccount, agency: switchedAgency)
+        self.amount = "R$ \(userAccount.balance)"
     }
     
-    
-    
     private static func createFormattedAccountWithAgencyString(account: String,
-                                                               agency: String) -> String {
+                                                               agency: String) -> String {        
         let accountPrefix = account.prefix(2)
         let accountMiddle = account.suffix(7).prefix(6)
         let accountDigit = account.suffix(1)
