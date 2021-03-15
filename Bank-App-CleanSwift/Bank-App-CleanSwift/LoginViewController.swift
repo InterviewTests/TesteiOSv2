d358d8d2e25b616201597617c64180d25b2a4af7
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-03-15 00:09:53 -0300

    Add rounded corners to button

--- Bank-App-CleanSwift/Bank-App-CleanSwift/LoginViewController.swift
+++ Bank-App-CleanSwift/Bank-App-CleanSwift/LoginViewController.swift
@@ -10,22 +10,36 @@ import UIKit
 class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+                            
+        self.roundButtonCorners()
         self.dismissKey()
+    }
+    
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
     }
 }
 
 // MARK: - Extension
 extension LoginViewController {
     /// Dismisses the keyboard when the app's user taps outside the textview
-    func dismissKey() {
+    private func dismissKey() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
         
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
     
-    @objc func dismissKeyboard() {
+    @objc private func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    private func roundButtonCorners() {
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
     }
 }
