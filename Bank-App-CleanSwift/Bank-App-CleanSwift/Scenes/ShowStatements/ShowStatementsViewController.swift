2d8a4b50dc649a45f86bde936db0d20040a1c594
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-03-18 13:01:19 -0300

    Fix error setValue:forUndefinedKey in userNameLabel

--- Bank-App-CleanSwift/Bank-App-CleanSwift/Scenes/ShowStatements/ShowStatementsViewController.swift
+++ Bank-App-CleanSwift/Bank-App-CleanSwift/Scenes/ShowStatements/ShowStatementsViewController.swift
@@ -8,33 +8,20 @@
 import UIKit
 
 class ShowStatementsViewController: UIViewController {
-
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userAccountLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    
+    @IBAction func exitButtonPressed(_ button: UIButton) {
+        print("Exiting...")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.hideNavigationBar()
-        
-//        let statusBarView = UIView(frame: CGRect(x: 0, y: 0, width:UIScreen.main.bounds.width, height: UIApplication.shared.statusBarFrame.height))
-//        statusBarView.backgroundColor = UIColor.magenta
-//        self.navigationController?.view.addSubview(statusBarView)
-//
-//        statusBarView.frame.height
     }
-    
-
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
