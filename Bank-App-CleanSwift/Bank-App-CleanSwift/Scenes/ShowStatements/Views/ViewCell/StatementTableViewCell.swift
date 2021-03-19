ee4b9212e1cda5b9326ecd375571c251ca48c7b5
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-03-19 11:29:51 -0300

    Create basic cell layout

---
+++ Bank-App-CleanSwift/Bank-App-CleanSwift/Scenes/ShowStatements/Views/ViewCell/StatementTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StatementTableViewCell.swift
+//  Bank-App-CleanSwift
+//
+//  Created by Adriano Rodrigues Vieira on 19/03/21.
+//
+
+import UIKit
+
+class StatementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
