1177e458323ba1fc8aec349adf8bcd14dea2c4e8
LeandroLee <>
2021-03-24 10:26:59 -0300

    #AdicionadoPODS

---
+++ BankTesteiOSv2/Pods/Target Support Files/Pods-BankTesteiOSv2/Pods-BankTesteiOSv2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BankTesteiOSv2 : NSObject
+@end
+@implementation PodsDummy_Pods_BankTesteiOSv2
+@end
---
+++ BankTesteiOSv2/Pods/Target Support Files/Pods-BankTesteiOSv2Tests/Pods-BankTesteiOSv2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BankTesteiOSv2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BankTesteiOSv2Tests
+@end
