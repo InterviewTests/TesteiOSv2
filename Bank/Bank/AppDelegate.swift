10c4774b8aeba8e2b60c5e8830b5085286251782
Gabriel Chacon <gabrielchaconbh@gmail.com>
2021-04-27 00:49:53 -0300

    AppDelegate com viewControler inicial

--- Bank/Bank/AppDelegate.swift
+++ Bank/Bank/AppDelegate.swift
@@ -10,12 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        let loginVC = LoginViewController()
+        let navController = UINavigationController(rootViewController: loginVC)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
 }
 
