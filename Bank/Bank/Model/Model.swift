6adffca93e77ca350694914295fa7d19b30d8593
Gabriel Chacon <gabrielchaconbh@gmail.com>
2021-04-27 00:38:53 -0300

    Criação de modelo User e protocolo Model base para conversão de modelos

---
+++ Bank/Bank/Model/Model.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public protocol Model: Codable, Equatable { }
+
+public extension Model {
+    func toData() -> Data? {
+        return try? JSONEncoder().encode(self)
+    }
+
+    func toJson() -> [String: Any]? {
+        guard let data = self.toData() else { return nil }
+        return try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+    }
+}
---
+++ Bank/Bank/Model/User.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public struct User: Model {
+    
+    public init(userId: String, name: String, bankAccount: String, agency: String, balance: Double) {
+        self.userId = userId
+        self.name = name
+        self.bankAccount = bankAccount
+        self.agency = agency
+        self.balance = balance
+    }
+    
+    let userId: String
+    let name: String
+    let bankAccount: String
+    let agency: String
+    let balance: Double
+    
+    public var id: String {
+        get {
+            userId
+        }
+    }
+    
+    
+    
+    func accountFormat() -> String {
+        
+        return "\(agency) / \(bankAccount)"
+    }
+}
