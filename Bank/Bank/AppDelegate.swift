63ea302e7ef10f0f921fd337dd661e37452cb0b3
Gabriel Chacon <gabrielchaconbh@gmail.com>
2021-04-27 00:26:03 -0300

    Projeto inicial

---
+++ Bank/Bank/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Bank
+//
+//  Created by Gabriel on 27/04/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Bank/Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank
+//
+//  Created by Gabriel on 27/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
