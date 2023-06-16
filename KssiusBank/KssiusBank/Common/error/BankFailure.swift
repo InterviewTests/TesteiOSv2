08a1308549a594a842dd44d8cd00c3e28dd27173
Cássio Sousa <cassio.sousa@accurate.com.br>
2023-06-16 11:09:54 -0300

    - Arquivos comuns da aplicação.

--- KssiusBank/KssiusBank/Common/error/BankFailure.swift
+++ KssiusBank/KssiusBank/Common/error/BankFailure.swift
@@ -1,7 +1,40 @@
 import Foundation
 
-enum BankFailure : Error {
+struct ErrorResponse: Codable, Equatable {
+
+    // MARK: - Properties
+
+    let code: Int?
+    let message: String?
+
+    // MARK: - Inits
+
+    init(code: Int? = nil, message: String? = nil) {
+        self.code = code
+        self.message = message
+    }
+
+    // MARK: - Equatable
+
+    static func == (lhs: ErrorResponse, rhs: ErrorResponse) -> Bool {
+        lhs.code == rhs.code && lhs.message == rhs.message
+    }
+}
+
+enum BankFailure: Error, Equatable {
+
+    // MARK: - Equatable
+
+    static func == (lhs: BankFailure, rhs: BankFailure) -> Bool {
+        switch (lhs, rhs) {
+        case (.number, .number): return true
+        case (.network(let lnet), .network(let rnet)):
+            return lnet == rnet
+        default:
+            return false
+        }
+    }
+
     case number
-    case network(code: Int)
-    case network(Error?)
+    case network(ErrorResponse?)
 }
