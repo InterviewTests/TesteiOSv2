c7203c486543471d829e705d5a2749c936956953
Matheus Pacheco Fusco <matheus.fusco@cddit.com.br>
2023-06-29 20:21:45 -0300

    Config inicial do projeto. Adicionando gitignore.

---
+++ MyBankApp/MyBankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBankApp
+//
+//  Created by Matheus Fusco on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
