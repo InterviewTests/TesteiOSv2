8b7a45b65868fe5ac81324bcac1de363af12a3d2
Matheus Pacheco Fusco <matheus.fusco@cddit.com.br>
2023-07-02 23:03:49 -0300

    LoginPresenterTests criados e LoginDisplayLogicSpy adicionado.

--- MyBankApp/MyBankApp/Login/Scene/LoginPresenter.swift
+++ MyBankApp/MyBankApp/Login/Scene/LoginPresenter.swift
@@ -3,6 +3,10 @@ import Foundation
 final class LoginPresenter: LoginPresentationLogic {
     weak var viewController: LoginDisplayLogic?
     
+    init(viewController: LoginDisplayLogic? = nil) {
+        self.viewController = viewController
+    }
+    
     func presentLoginSuccess() {
         viewController?.displayLoginSuccess()
     }
